4bb788560015ff2e1f0031ae3a9f685187d90b91
WAY-ADVENTURER <2992850181@qq.com>
2025-03-19 12:17:18 +0800

    上传博客

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742357641',resources: [
+    cacheName: 'chirpy-1742357804',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
