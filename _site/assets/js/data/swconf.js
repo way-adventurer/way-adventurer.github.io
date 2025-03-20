c356f0a86cbb9a6f3612d51c3753bcac799e858a
WAY-ADVENTURER <2992850181@qq.com>
2025-03-20 20:24:39 +0800

    更新博客

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742380289',resources: [
+    cacheName: 'chirpy-1742473440',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
