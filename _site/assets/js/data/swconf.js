a5dfe5d3003164cd95d2aab3a2834028a6c8084a
WAY-ADVENTURER <2992850181@qq.com>
2025-04-14 22:54:54 +0800

    增加分页器

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744640248',resources: [
+    cacheName: 'chirpy-1744642262',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
